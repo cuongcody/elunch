85c82d9a6825e0c834b2edabdb9972ddc42b88ee
Cuong (Cody) N. NGUYEN <cody@enclave.vn>
2015-11-12 11:07:33 +0700

    asd

--- application/language/english/web_portal/gen_log_file_meal_lang.php
+++ application/language/english/web_portal/gen_log_file_meal_lang.php
@@ -2,5 +2,5 @@
     $lang["gen_log_file_failure"] = "Generate a log file meal failed";
     $lang["gen_log_file_success"] = "Generate a log file meal successfully";
     $lang["cannot_find_log_file"] = "Could not find the report data";
-    $lang["no_data_in_log_file"] = "Could not see data in the report";
+    $lang["no_data_in_log_file"] = "No data in the report";
  ?>
