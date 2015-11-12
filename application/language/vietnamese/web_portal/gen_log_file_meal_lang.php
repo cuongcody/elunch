8830d054444e8830184b08a2cfd66728e5eba73e
Cuong (Cody) N. NGUYEN <cody@enclave.vn>
2015-11-12 10:32:24 +0700

    asd

--- application/language/vietnamese/web_portal/gen_log_file_meal_lang.php
+++ application/language/vietnamese/web_portal/gen_log_file_meal_lang.php
@@ -1,4 +1,6 @@
 <?php
     $lang["gen_log_file_failure"] = "Tạo tập tin log thất bại";
     $lang["gen_log_file_success"] = "Tạo tập tin log thành công";
+    $lang["cannot_find_log_file"] = "Không tìm thấy bản báo cáo";
+    $lang["no_data_in_log_file"] = "Không có dữ liệu trong báo cáo";
  ?>
