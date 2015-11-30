275a0a23676443ae4023f2068c67417cd7086c96
Cuong (Cody) N. NGUYEN <cody@enclave.vn>
2015-11-30 14:30:49 +0700

    asd

--- application/models/Dishes_model.php
+++ application/models/Dishes_model.php
@@ -87,16 +87,18 @@ class Dishes_model extends CI_Model {
         $date_from = new DateTime($date);
         $date_to = new DateTime($date);
         $date_to->add(new DateInterval('P'.$number_of_days.'D'));
-        $this->db->select('dishes.*, pictures.image, pictures.dish_id, meals.meal_date, meals.for_vegans');
+        $this->db->select('dishes.*, categories.id, pictures.image, pictures.dish_id, meals.meal_date, meals.for_vegans');
         $this->db->from('meals');
         $this->db->join('menus', 'meals.menu_id = menus.id');
         $this->db->join('dishes_menus', 'dishes_menus.menu_id = menus.id');
         $this->db->join('dishes', 'dishes_menus.dish_id = dishes.id');
         $this->db->join('pictures', 'dishes.id = pictures.dish_id');
+        $this->db->join('categories', 'categories.id = dishes.category_id');
         $this->db->where('meal_date >=', $date_from->format('Y-m-d'));
         $this->db->where('meal_date <=', $date_to->format('Y-m-d'));
         $this->db->order_by('meal_date', 'asc');
         $this->db->order_by('menus.id', 'asc');
+        $this->db->order_by('categories.id', 'asc');
         $dishes_grouped_by_date = array();
         $dishes = $this->db->get()->result();
         if ($dishes != NULL)
