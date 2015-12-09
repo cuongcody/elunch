7bbbf01efbbe3931e78854ab65a3621ac211d666
Cuong (Cody) N. NGUYEN <cody@enclave.vn>
2015-12-09 09:18:13 +0700

    asd

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://113.160.225.87:8484/';
+$config['base_url'] = 'http://113.160.225.76:8989/elunch/';
 
 /*
 |--------------------------------------------------------------------------
