cb32b499b5dce3021b493672807d00338b97c08e
Cuong (Cody) N. NGUYEN <cody@enclave.vn>
2016-02-22 09:50:23 +0700

    edit language

--- application/language/english/server_api/dishes_api_lang.php
+++ application/language/english/server_api/dishes_api_lang.php
@@ -3,6 +3,6 @@
     $lang["get_dishes_failure"] = "Not available";
     $lang["get_meals_success"] = "Get meals successfully";
     $lang["get_meals_failure"] = "Not available";
-    $lang["vegan_meal"] = "Món chay";
+    $lang["vegan_meal"] = "Suất chay";
     $lang["get_meals_not_valid"] = "The request failed because the data sent in the request is invalid: 'from' or 'days'";
 ?>
